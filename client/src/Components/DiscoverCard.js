a028c83f158077c02da1eaa52c1a975b579701e7
PascalUlor <pascalulor@yahoo.com>
2019-06-24 23:34:24 +0100

    ft(style card compnent for listings)

--- client/src/Components/DiscoverCard.js
+++ client/src/Components/DiscoverCard.js
@@ -1,20 +1,104 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
+import {slate_grey} from './~reusables/variables/colors';
+import {body_2, body_1} from './~reusables/variables/font-sizes';
 
+const Wrap = styled.div`
+  /* Rectangle */
 
-const DiscoverCard = ({ data }) =>{
-    console.log('==========',data.name);
-    const info = data.education? data.education: data.category;
-    // const listView = data.jobs? data.jobs: data.experience;
+  margin: 0 auto;
+  width: 1260px;
+  height: 680px;
+  left: 180px;
+  top: 220px;
 
-    return(
-        <div>
-            <p>{data.name}</p>
+  /* White */
+  background: #ffffff;
+`;
+const Card = styled.div`
+  /* Background */
+
+  position: absolute;
+  width: 350px;
+  height: 560px;
+  left: 636px;
+  top: 292px;
+
+  /* White */
+  background: #ffffff;
+  /* Faded Black */
+  border: 1px solid ${slate_grey};
+  box-sizing: border-box;
+  border-radius: 8px;
+  margin: 0 auto;
+  text-align: center;
+  line-height: 20px;
+  font-size: ${body_2};
+`;
+
+const ImageContainer = styled.div`
+  background-color: #97a2b9;
+  border-radius: 100%;
+  height: 8rem;
+  width: 50%;
+  margin: 1rem auto;
+`;
+
+const Header = styled.div`
+  font-size: ${body_1};
+`;
+const SubHeader = styled.div`
+font-size: ${body_1};
+color: ${slate_grey};
+`;
+
+const DetailSection = styled.div`
+border-top: 1px solid ${slate_grey};
+  margin: 0 auto;
+  text-align: center;
+  line-height: 1.5rem;
+  font-size: ${body_2};
+`;
+
+const DiscoverCard = ({ data }) => {
+  const infoHeader = data.qualifications ? "Qualifications" : 'Category';
+  const info = data.qualifications ? data.qualifications : data.category;
+  const listView = data.jobs ? data.jobs : data.experience;
+  console.log(listView);
+
+  return (
+    <Wrap>
+      {listView.map(item => {
+        return( <Card>
+          <ImageContainer />
+          
+            <Header>{data.name}</Header>
+            <SubHeader>{item.title}</SubHeader>
             <p>{data.email}</p>
             <p>{data.location}</p>
+          
+          <DetailSection>
+          <SubHeader>About</SubHeader>
+            <p>{data.about}</p>
+          </DetailSection>
+
+          <DetailSection>
+          <SubHeader>{infoHeader}</SubHeader>
             <p>{info}</p>
-            {/* <p>{listView}</p> */}
-        </div>
-    )
-}
+            </DetailSection>
+            <DetailSection>
+            <SubHeader>Description</SubHeader>
+            <ul>
+              <li>{item.experience || item.company}</li>
+              <li>{item.description}</li>
+            </ul>
+            
+          </DetailSection>
+        </Card>
+        )
+      })}
+    </Wrap>
+  );
+};
 
 export default DiscoverCard;
--- client/src/mock/index.js
+++ client/src/mock/index.js
@@ -9,20 +9,13 @@ const listings = [
         email: "slimtrader@gmail.com",
         category: "IT",
         location: "123 V.I Lagos",
-        jobs: [
-          {
+        jobs: [{
             id: uuid(),
-            title: "software-dev",
+            title: "software-deve",
             description: "Business Details must be between 20 to 1000 characters",
             experience: "2 years"
-          },
-          {
-            id: uuid(),
-            title: "PM",
-            description: "Business Details must be between 20 to 1000 characters",
-            experience: "2 years"
-          }
-        ]
+            }],
+        about: "Business Details must be between 20 to 1000 characters"
       },
       {
         id: uuid(),
@@ -30,20 +23,13 @@ const listings = [
         email: "interswitch@gmail.com",
         category: "finance",
         location: "40 V.I Lagos",
-        jobs: [
-          {
+        jobs: [{
             id: uuid(),
             title: "software-dev",
             description: "Business Details must be between 20 to 1000 characters",
             experience: "2 years"
-          },
-          {
-            id: uuid(),
-            title: "HR",
-            description: "Business Details must be between 20 to 1000 characters",
-            experience: "2 years"
-          }
-        ]
+          }],
+        about: "Business Details must be between 20 to 1000 characters"
       }
     ]
   },
@@ -54,32 +40,28 @@ const listings = [
         id: uuid(),
         name: "Pascal",
         email: "pascal@gmail.com",
-        education: "UI",
+        qualifications: "UI",
         location: "123 V.I Lagos",
-        experience: [
-          {
+        experience: [{
             id: uuid(),
             title: "HR",
             company: "HFP",
             description: "Business Details must be between 20 to 1000 characters"
-          }
-        ],
+          }],
         about: "Business Details must be between 20 to 1000 characters"
       },
       {
         id: uuid(),
         name: "Alfred",
         email: "interswitch@gmail.com",
-        education: "stanford",
+        qualifications: "stanford",
         location: "40 V.I Lagos",
-        experience: [
-          {
+        experience: [{
             id: uuid(),
             title: "HR",
             company: "HFP",
             description: "Business Details must be between 20 to 1000 characters"
-          }
-        ],
+          }],
         about: "Business Details must be between 20 to 1000 characters",
       }
     ]
