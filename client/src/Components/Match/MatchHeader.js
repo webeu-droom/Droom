f3c1c01d4fb50530411e6fc339b7583ed5d06b86
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-06-24 14:21:51 +0100

    Add Header for match

--- client/src/Components/Match/MatchHeader.js
+++ client/src/Components/Match/MatchHeader.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
-import { light_grey } from '../~reusables/variables/colors'
+import { light_grey, black } from '../~reusables/variables/colors'
+import { medium_space_3 } from '../~reusables/variables/spacing'
+import {Dropdown} from '../~reusables/atoms/Dropdowns'
 
-const LayoutHeader = () => {
+const MatchHeader = () => {
     return (
         <StyleMH>
-            
+            {/* Needs a conditional checking for company or candidate */}
+            <h1>Companies</h1>
+            <Dropdown>
+                <option value="">Full Stack Web Developer</option>
+            </Dropdown>
         </StyleMH>
     )
 }
@@ -15,8 +21,22 @@ const StyleMH = styled.div`
     min-height: 100px;
     max-height: 200px;
     height: 20vh;
+    color: ${black};
+
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    h1 {
+        margin: auto 0;
+        padding-left: ${medium_space_3};
+    }
+
+    select {
+        margin-right: ${medium_space_3};
+    }
 
     background-color: ${light_grey};
 `
 
-export default LayoutHeader;
+export default MatchHeader;
