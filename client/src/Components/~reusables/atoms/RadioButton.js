4cbe88fb692506ff8aa276ff71b28e8870ad20ef
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-06-25 08:13:59 +0100

    Add styling to register page

--- client/src/Components/~reusables/atoms/RadioButton.js
+++ client/src/Components/~reusables/atoms/RadioButton.js
@@ -1,16 +1,18 @@
 import styled from "styled-components";
-import { extra_small_space, small_space, medium_space_1 } from "../variables/spacing";
+import {
+  extra_small_space,
+  small_space,
+  medium_space_1
+} from "../variables/spacing";
 import { white, blue, black } from "../variables/colors";
 import { button_text } from "../variables/font-sizes";
 import { source_sans_pro } from "../variables/font-family";
 
-export const Input = styled.input`
+export const RadioButton = styled.fieldset`
   font-size: ${button_text};
   font-family: ${source_sans_pro};
-
   box-shadow: 0 0.8rem 2.5rem 0 rgba(40, 51, 63, 0.05);
   transition: all 100ms ease-in-out;
-  height: 40px;
   width: 100%;
   background-color: transparent;
   border: 1px solid #eaeaea;
@@ -19,6 +21,16 @@ export const Input = styled.input`
   font-size: 16px;
   margin-bottom: ${medium_space_1};
   color: ${black};
+
+  div {
+    display: inline-block;
+    margin-right: ${small_space};
+
+    input,
+    label {
+      vertical-align: middle;
+    }
+  }
 
   &:active {
     opacity: 0.8;
