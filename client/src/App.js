9364ac026b0284d699b1004ccdfc9c2f381c4330
Thorben Bender <bender.thorben0@gmail.com>
2019-06-26 18:09:06 +0200

    resolved merge conflicts

--- client/src/App.js
+++ client/src/App.js
@@ -7,59 +7,29 @@ import { firebaseConnect, withFirestore } from "react-redux-firebase";
 import Login from "./Components/views/Login/Login";
 import Register from "./Components/views/Register/Register";
 import Home from "./Components/Home";
-import Profile from "./Components/views/Profile/Profile"
+import Profile from "./Components/views/Profile/Profile";
 
 import DiscoverPage from "./Components/views/Discover/DiscoverPage";
 
-<<<<<<< HEAD
-import Match from "./Components/Match/Match";
-import Chat from "./Components/Chat/Chat";
-import Landing from "./Components/Landing/Landing";
-import Listing from "./Components/JobListing/Listing";
-import CreateListing from "./Components/JobListing/CreateListing";
-=======
 import Match from "./Components/views/Match/Match";
 import Chat from "./Components/views/Chat/Chat";
 import Landing from "./Components/views/Landing/Landing";
 import Listing from "./Components/views/JobListing/Listing";
->>>>>>> f4f6ca60ba6b70c0efaf509e7d86a6d90ebe1107
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <Route
-          exact
-          path="/discover/:type?"
-          render={props => <DiscoverPage {...props} />}
-        />
+        <Route exact path="/discover/:type?" render={props => <DiscoverPage {...props} />} />
 
         <Route exact path="/" render={props => <Landing {...props} />} />
         <Route exact path="/login" render={props => <Login {...props} />} />
-        <Route
-          exact
-          path="/register"
-          render={props => <Register {...props} />}
-        />
+        <Route exact path="/register" render={props => <Register {...props} />} />
         <Route exact path="/home" render={props => <Home {...props} />} />
         <Route exact path="/profile" render={props => <Profile {...props} />} />
         <Route exact path="/match" render={props => <Match {...props} />} />
-<<<<<<< HEAD
         <Route exact path="/match/chat/:id" render={props => <Chat {...props} />} />
         <Route exact path="/profile/listing/:id" render={props => <Listing {...props} />} />
-        <Route exact path="/profile/createListing" render={props => <CreateListing {...props} />} />
-=======
-        <Route
-          exact
-          path="/match/chat/:id"
-          render={props => <Chat {...props} />}
-        />
-        <Route
-          exact
-          path="/profile/listing/:id"
-          render={props => <Listing {...props} />}
-        />
->>>>>>> f4f6ca60ba6b70c0efaf509e7d86a6d90ebe1107
       </div>
     );
   }
