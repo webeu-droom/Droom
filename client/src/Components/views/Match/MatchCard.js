a748adf1ac15274a54321a0d4c9651835b5bf0ee
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-06-27 16:42:36 +0100

    Give matchcard a max width of 1000 px

--- client/src/Components/views/Match/MatchCard.js
+++ client/src/Components/views/Match/MatchCard.js
@@ -51,6 +51,7 @@ const MatchCard = ({ image, message, name, title, location, matchesId }) => {
 
 const StyledMatchCard = styled(animated.div)`
   flex: 1 1 500px;
+  max-width: 1000px;
   border: 1px solid ${slate_grey};
   border-radius: 2px;
   margin: ${medium_space_1} ${medium_space_1} 0 ${medium_space_1};
