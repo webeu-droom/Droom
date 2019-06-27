81ca4d67123e48f911385cacbe8d24504114af8f
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-06-27 16:24:37 +0100

    Create reusable popup component

---
+++ client/src/Components/~reusables/components/Popup.js
@@ -0,0 +1,68 @@
+import React from "react";
+import styled from "styled-components";
+import { ButtonPrimary } from "../atoms/Buttons";
+import { source_sans_pro } from "../variables/font-family";
+import { black, faded_blue } from "../variables/colors";
+import { heading_3, heading_4 } from "../variables/font-sizes";
+
+class Popup extends React.Component {
+  render() {
+    return (
+      <StyledPopup>
+        <div className="popup">
+          <div className="popup-inner">
+            <h4>{this.props.text}</h4>
+            <ButtonPrimary onClick={this.props.closePopup}>OK</ButtonPrimary>
+          </div>
+        </div>
+      </StyledPopup>
+    );
+  }
+}
+
+const StyledPopup = styled.div`
+  z-index: 100;
+
+  h4 {
+    text-align: center;
+    font-family: ${source_sans_pro};
+    color: ${black};
+    font-size: ${heading_4};
+  }
+
+  .popup {
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    margin: auto;
+    background-color: rgba(0, 0, 0, 0.5);
+  }
+  .popup-inner {
+    background: ${faded_blue};
+    position: absolute;
+    left: 25%;
+    right: 25%;
+    top: 25%;
+    bottom: 25%;
+    margin: auto;
+    border-radius: 20px;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  @media only screen and (max-width: 500px) {
+    .popup-inner {
+      left: 15%;
+      right: 15%;
+    }
+  }
+`;
+
+export default Popup;
