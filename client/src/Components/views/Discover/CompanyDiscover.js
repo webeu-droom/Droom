25a0e47f74ed94ba5c35eb8871da8b991b84866d
PascalUlor <pascalulor@yahoo.com>
2019-07-05 21:22:36 +0100

    install react-spring amd react-with-gesture

--- client/src/Components/views/Discover/CompanyDiscover.js
+++ client/src/Components/views/Discover/CompanyDiscover.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import Loader from "react-loader-spinner";
 import { blue } from "../../~reusables/variables/colors";
 import { ButtonPrimary, ButtonTertiary } from "../../~reusables/atoms/Buttons";
 import DiscoverHeader from "./DiscoverHeader";
